650560c99c10ed6f3a58f5431c37e1975115f6e8
Emily Caples <etcaples@gmail.com>
2018-07-31 11:24:45 -0700

    destructured props in ItemEntryFields

--- client/src/components/ItemEntryFields.jsx
+++ client/src/components/ItemEntryFields.jsx
@@ -1,37 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ItemEntryFields = props => (
-  <div>
+const ItemEntryFields = (props) => {
+  const { friends } = props;
+  return (
     <div>
-      <input onChange={e => props.handleFriendChange(e)} placeholder="Add friend..." />
-      <button type="button" onClick={() => props.handleFriendSubmit()}>
-        Submit
-      </button>
+      <div>
+        <input onChange={e => props.handleFriendChange(e)} placeholder="Add friend..." />
+        <button type="button" onClick={() => props.handleFriendSubmit()}>
+          Submit
+        </button>
+      </div>
+      <h4>
+        {'Manual entry...'}
+      </h4>
+      <div>
+        <input placeholder="item" />
+        {'$'}
+        <input placeholder="price" />
+        <select>
+          {
+            { friends }.map(friend => (
+              <option>
+                {friend}
+              </option>
+            ))
+          }
+        </select>
+        <button type="button">
+          {'Submit'}
+        </button>
+      </div>
     </div>
-    <h4>
-      {'Manual entry...'}
-    </h4>
-    <div>
-      <input placeholder="item" />
-      {'$'}
-      <input placeholder="price" />
-      <select>
-        {
-          props.friends.map(friend => (
-            <option>
-              {friend}
-            </option>
-          ))
-        }
-      </select>
-      <button type="button">
-        {'Submit'}
-      </button>
-    </div>
-
-  </div>
-);
+  );
+};
 
 ItemEntryFields.propTypes = {
   friends: PropTypes.arrayOf(PropTypes.string).isRequired,
