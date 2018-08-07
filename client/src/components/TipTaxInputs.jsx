e5d9cce292aa71fed1eb1a910d69ca5aa3f600ff
Emily Caples <etcaples@gmail.com>
2018-08-07 11:02:12 -0700

    added button and change/click handlers to tax & tip inputs

--- client/src/components/TipTaxInputs.jsx
+++ client/src/components/TipTaxInputs.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TaxTipInputs = () => {
+const TaxTipInputs = (props) => {
+  const {
+    getTax,
+    getTip,
+    combineTaxTip,
+  } = props;
   return (
     <div>
       <div>
-        <input placeholder="add tax & other fees..." />
+        <input onChange={() => getTax()} placeholder="add tax & other fees..." />
       </div>
       <div>
-        <input placeholder="add tip..." />
+        <input onChange={() => getTip()} placeholder="add tip..." />
+      </div>
+      <div>
+        <button onClick={() => combineTaxTip()} type="button">
+          Finalize Tax/Tip
+        </button>
       </div>
     </div>
   );
 };
 
 TaxTipInputs.propTypes = {
-
+  getTax: PropTypes.func.isRequired,
+  getTip: PropTypes.func.isRequired,
+  combineTaxTip: PropTypes.func.isRequired,
 };
 
 export default TaxTipInputs;
