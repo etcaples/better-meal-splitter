ca343f440987dd6110e7c08d0c015ef59b4b3f32
Emily Caples <etcaples@gmail.com>
2018-09-20 13:36:07 -0700

    added public folder to gitignore; added tbody and thead to dynamic html table

--- client/src/components/ItemList.jsx
+++ client/src/components/ItemList.jsx
@@ -6,40 +6,44 @@ const ItemList = (props) => {
   return (
     <div>
       <table>
-        <tr>
-          <th>
-            Item
-          </th>
-          <th>
-            Price
-          </th>
-          <th>
-            Friends
-          </th>
-        </tr>
-        {
-          itemDetails.map(([item, price, friends]) => (
-            <tr>
-              <td>
-                {item}
-              </td>
-              <td>
-                {`$${price}`}
-              </td>
-              <td>
-                {friends.map(friend => (
-                  <p>
-                    {friend}
-                  </p>
-                ))}
-              </td>
-              <td>
-                <button type="button" onClick={() => removeItemRow([item, price, friends])}>
-                  Remove
-                </button>
-              </td>
-            </tr>))
-        }
+        <thead>
+          <tr>
+            <th>
+              Item
+            </th>
+            <th>
+              Price
+            </th>
+            <th>
+              Friends
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            itemDetails.map(([item, price, friends]) => (
+              <tr>
+                <td>
+                  {item}
+                </td>
+                <td>
+                  {`$${price}`}
+                </td>
+                <td>
+                  {friends.map(friend => (
+                    <p>
+                      {friend}
+                    </p>
+                  ))}
+                </td>
+                <td>
+                  <button type="button" onClick={() => removeItemRow([item, price, friends])}>
+                    Remove
+                  </button>
+                </td>
+              </tr>))
+          }
+        </tbody>
       </table>
       <button type="button" onClick={() => setTallySubtotals()}>
         Confirm Item Details (Get Subtotals)
